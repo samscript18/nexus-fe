5d3b66255330840168acf0b950f292bcfe191b73
Adesugba Samuel <97664905+samscript18@users.noreply.github.com>
2024-08-13 15:42:20 +0100

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,9 +9,9 @@ export default function AuthLayout({ children }: Props) {
   return (
     <>
       <Navbar />
-      <section className="w-full min-h-screen flex items-center justify-center z-[4] pb-[60px] pt-[60px]">
+      <section className="w-full min-h-screen bg-[#000] flex items-center justify-center z-[4] pb-[60px] pt-[60px]">
         <div
-          className="relative w-[96vw] max-w-[400px] py-8 px-8 md:px-14 bg-[#7499da] overflow-hidden rounded-sm z-[4]"
+          className="relative w-[96vw] max-w-[400px] py-8 px-8 md:px-14 bg-[#004080] overflow-hidden rounded-sm z-[4]"
           style={{
             background:
               "radial-gradient(circle, rgba(76,164,207,1) 35%, rgba(135,148,180,1) 100%)",
